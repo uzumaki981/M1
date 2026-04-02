af0a93455b818a429673a890ee10c486b5cfb17b
uzumaki981 <mdmahim2042007@gmail.com>
2026-04-02 18:22:47 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -13,7 +13,7 @@ module.exports = {
 
   onStart: async function({ message }) {
     const text = "✓ | Here is the updated repository:\n\nhttps://github.com/NeoKEX/Goatbot-updated.git\n\n" +
-                 "Changes:\n1. No Google Credentials needed\n2. Enhanced overall performance\n3. Now using @neoaz07/nkxfca(v1.0.4)\n4. Working on all groups\n5. Id Ban Issue solved 90% and running for a long time\n\nNB: If you want to use @neoaz07/nkxfca please install by typing: npm i @neoaz07/nkxfca@latest\n\n" +
+                 "Changes:\n1. No Google Credentials needed\n2. Enhanced overall performance\n3. Now using @neoaz07/nkxfca(v1.0.9)\n4. Working on all groups\n5. Id Ban Issue solved 90% and running for a long time (No logout issue)\n\nNB: If you want to use @neoaz07/nkxfca please install by typing: npm i @neoaz07/nkxfca@latest\n\n" +
                  "Keep supporting^_^";
     
     message.reply(text);
